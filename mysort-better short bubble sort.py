ae4d657ad4c7988b6a451835643c270c3928f622
Wael Al-Atrash <wassabi.dietaube@gmail.com>
2017-12-25 19:13:44 +0100

    camelCase

--- mysort-better short bubble sort.py
+++ mysort-better short bubble sort.py
@@ -2,26 +2,26 @@ import time
 start_time = time.time()
 
 
-def myShortBubbleSort(alist):
+def myShortBubbleSort(aList):
     exchanges = True
-    passnum = (len(alist) - 1)
+    passNum = (len(aList) - 1)
     x = 0
-    while passnum > 0 and exchanges:
+    while passNum > 0 and exchanges:
         exchanges = False
-        for i in range(x, passnum):
-            if alist[i] > alist[i + 1]:
+        for i in range(x, passNum):
+            if aList[i] > aList[i + 1]:
                 exchanges = True
-                temp = alist[i]
-                alist[i] = alist[i + 1]
-                alist[i + 1] = temp
-    for i in range(passnum - 1, x, -1):
-        if alist[i] > alist[i + 1]:
-            exchanges = True
-            temp = alist[i + 1]
-            alist[i + 1] = alist[i]
-            alist[i] = temp
-        x += 1
-    passnum = passnum - 1
+                temp = aList[i]
+                aList[i] = aList[i + 1]
+                aList[i + 1] = temp
+        for i in range(passNum - 1, x, -1):
+            if aList[i] > aList[i + 1]:
+                exchanges = True
+                temp = aList[i + 1]
+                aList[i + 1] = aList[i]
+                aList[i] = temp
+            x += 1
+    passNum = passNum - 1
 
 
 alist1 = [93, 54, 26, 62, 17, 77, 31, 44, 55, 20, 10]
